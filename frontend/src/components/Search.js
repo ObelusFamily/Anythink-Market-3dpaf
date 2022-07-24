6d723b176f80fadb5e67c13e6dd9042f21c3269f
bkkcreative19 <bkkcreative16@gmail.com>
2022-07-24 16:12:02 -0500

    added search functionality on frontend

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -5,7 +5,7 @@ const Search = ({onSearchChange}) => {
   
 return (
 
-    <div className='search-box'>
+    <div className='search-box' id='search-box'>
         <input type="text" onChange={(e) => onSearchChange(e.target.value)} />
         <i  className="bi bi-search"></i>
     </div>
