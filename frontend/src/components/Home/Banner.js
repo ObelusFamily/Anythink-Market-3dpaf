7ed7c11ca4ec6276f1eb52c0947d67030b4851c1
bkkcreative19 <bkkcreative16@gmail.com>
2022-07-24 20:47:18 -0500

    added input invisible unless get is clicked

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "../search-box";
 
 const Banner = ({onSearchChange}) => {
+  const [isVisible, setIsVisible] = useState(false)
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div className="d-flex justify-content-center align-items-center">
-          <span id="get-part">A place to get</span>
-          <SearchBox onSearchChange={onSearchChange}/>
+          <span id="get-part">A place to <span onClick={() => setIsVisible(!isVisible)}>get </span></span>
+          {isVisible && <SearchBox onSearchChange={onSearchChange}/>}
+         
     
           <span> the cool stuff.</span>
         </div>
