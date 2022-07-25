dd102d3a76e028af64315fe719a485ac5dbbe854
bkkcreative19 <bkkcreative16@gmail.com>
2022-07-25 08:05:01 -0500

    bug

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -3,13 +3,13 @@ import logo from "../../imgs/logo.png";
 import SearchBox from "../search-box";
 
 const Banner = ({onSearchChange}) => {
-  const [isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(false)
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div className="d-flex justify-content-center align-items-center">
-          <span id="get-part">A place to <span id="get-part" onClick={() => setIsVisible(!isVisible)}>get </span></span>
+          <span>A place to <span id="get-part" onClick={() => setIsVisible(!isVisible)}>get </span></span>
           {isVisible && <SearchBox onSearchChange={onSearchChange}/>}
          
     
